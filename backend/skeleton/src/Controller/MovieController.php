6cdfdf1b5ef5ef622360909f90e939d5987ee188
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-06-24 11:05:28 +0200

    Added POST, PUT and DELETE

--- backend/skeleton/src/Controller/MovieController.php
+++ backend/skeleton/src/Controller/MovieController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Enum\Genre;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
     #[Route('/movies', name: 'api_movies', methods: ['GET'])]
-    public function getMovies(EntityManagerInterface $em)
+    public function getMovies(EntityManagerInterface $em): JsonResponse
     {
         $movies = $em->getRepository(Movie::class)->findAll();
 
@@ -18,7 +22,7 @@ class MovieController extends AbstractController
             'id' => $movie->getId(),
             'title' => $movie->getTitle(),
             'description' => $movie->getDescription(),
-            'genre' => $movie->getGenre(),
+            'genre' => $movie->getGenre()?->value,
             'duration' => $movie->getDuration(),
             'release_date' => $movie->getReleaseDate()->format('c'),
         ], $movies);
@@ -27,10 +31,100 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movies/{id}', name: 'api_movie', methods: ['GET'])]
-    public function getMovie(EntityManagerInterface $em, $id)
+    public function getMovie(EntityManagerInterface $em, $id): JsonResponse
     {
         $movie = $em->getRepository(Movie::class)->find($id);
 
-        return $this->json($movie);
+        if (!$movie) {
+            return $this->json(['error' => 'Movie not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json([
+            'id' => $movie->getId(),
+            'title' => $movie->getTitle(),
+            'description' => $movie->getDescription(),
+            'genre' => $movie->getGenre()?->value,
+            'duration' => $movie->getDuration(),
+            'release_date' => $movie->getReleaseDate()->format('c'),
+        ]);
+    }
+
+    #[Route('/movies', name: 'api_movie_create', methods: ['POST'])]
+    public function createMovie(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['title'], $data['genre'], $data['release_date'], $data['duration'])) {
+            return $this->json(['error' => 'Missing required fields'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!Genre::tryFrom($data['genre'])) {
+            return $this->json(['error' => 'Invalid genre value'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $movie = new Movie();
+        $movie->setTitle($data['title'])
+            ->setDescription($data['description'] ?? null)
+            ->setGenre(Genre::from($data['genre']))
+            ->setDuration((int)$data['duration'])
+            ->setReleaseDate(new \DateTime($data['release_date']));
+
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->json(['message' => 'Movie created', 'id' => $movie->getId()], Response::HTTP_CREATED);
+    }
+
+    #[Route('/movies/{id}', name: 'api_movie_update', methods: ['PUT'])]
+    public function updateMovie(Request $request, EntityManagerInterface $em, $id): JsonResponse
+    {
+        $movie = $em->getRepository(Movie::class)->find($id);
+        if (!$movie) {
+            return $this->json(['error' => 'Movie not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['title'])) {
+            $movie->setTitle($data['title']);
+        }
+
+        if (isset($data['description'])) {
+            $movie->setDescription($data['description']);
+        }
+
+        if (isset($data['genre'])) {
+            $genre = Genre::tryFrom($data['genre']);
+            if (!$genre) {
+                return $this->json(['error' => 'Invalid genre value'], Response::HTTP_BAD_REQUEST);
+            }
+            $movie->setGenre($genre);
+        }
+
+        if (isset($data['duration'])) {
+            $movie->setDuration((int)$data['duration']);
+        }
+
+        if (isset($data['release_date'])) {
+            $movie->setReleaseDate(new \DateTime($data['release_date']));
+        }
+
+        $em->flush();
+
+        return $this->json(['message' => 'Movie updated']);
+    }
+
+    #[Route('/movies/{id}', name: 'api_movie_delete', methods: ['DELETE'])]
+    public function deleteMovie(EntityManagerInterface $em, $id): JsonResponse
+    {
+        $movie = $em->getRepository(Movie::class)->find($id);
+        if (!$movie) {
+            return $this->json(['error' => 'Movie not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $em->remove($movie);
+        $em->flush();
+
+        return $this->json(['message' => 'Movie deleted']);
     }
 }
