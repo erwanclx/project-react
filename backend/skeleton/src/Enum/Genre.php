807650f5e382c43fcc5bfe59536733919d6b16e7
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-06-24 14:18:53 +0200

    Added lot of genres

--- backend/skeleton/src/Enum/Genre.php
+++ backend/skeleton/src/Enum/Genre.php
@@ -5,8 +5,21 @@ namespace App\Enum;
 enum Genre: string
 {
     case ACTION = 'Action';
-    case DRAMA = 'Drama';
+    case ADVENTURE = 'Adventure';
+    case ANIMATION = 'Animation';
+    case BIOGRAPHY = 'Biography';
     case COMEDY = 'Comedy';
+    case DOCUMENTARY = 'Documentary';
+    case DRAMA = 'Drama';
+    case FICTION = 'Fiction';
+    case FANTASY = 'Fantasy';
+    case HISTORY = 'History';
     case HORROR = 'Horror';
+    case MUSIC = 'Music';
+    case POLICE = 'Police';
+    case ROMANCE = 'Romance';
     case SCIFI = 'Sci-Fi';
+    case THRILLER = 'Thriller';
+    case WAR = 'War';
+    case WESTERN = 'Western';
 }
